f603b0d9e7ef1cf8264d13128fb6ac8178a7ebe2
Adrienne Yao <adrienne.yao@icloud.com>
2019-09-12 02:10:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Adrienne."
+  end
+
+  get '/hometown' do
+    "My hometown is Atlanta, GA."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Umbilical Moonrise by Lotus."
+  end
 end
